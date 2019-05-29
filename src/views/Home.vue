c4ffee914be2a6345d427131b51e455c6adc6476
斌斌斌 <1587443838@qq.com>
2019-05-29 21:13:12 +0800

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    aa
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Epub from 'epubjs'
+global.ePub = Epub
+
+export default {
+  name: 'home',
+  mounted(){
+      this.book = new Epub('');
+      this.book.renderTo('read',{
+          width:window.innerWidth,
+          height:window.innerHeight0
+      }).display()
+  }
+}
+</script>
---
+++ vue.config.js
@@ -0,0 +1,8 @@
+/**
+ * Created by cyb on 2019/5/28.
+ */
+module.exports = {
+  baseUrl:process.env.NODE_ENV === 'production'
+   ? './'
+    : '/'
+}
